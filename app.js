25baa7fe85df39188798bb5514d11da1c272b8da
Amaan <amaanshaikh.gg@gmail.com>
2024-06-20 00:09:28 +0530

    level up logic

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ letuserSeq = [];
 let started = false;
 let level = 0;
 
+let h2 = document.querySelector("h2");
+
 document.addEventListener("keypress", function () {
-  if (statred == false) {
+  if (started == false) {
     console.log("game is started");
     started = true;
+
+    levelUp();
   }
 });
+
+function levelUp() {
+  level++;
+  h2.innerText = `Level ${level}`;
+}
