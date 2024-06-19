21dc970e125602d8e78b9b811bb306da66b95e04
Amaan <amaanshaikh.gg@gmail.com>
2024-06-19 23:59:32 +0530

    game start logic

---
+++ app.js
@@ -0,0 +1,12 @@
+let gameSeq = [];
+letuserSeq = [];
+
+let started = false;
+let level = 0;
+
+document.addEventListener("keypress", function () {
+  if (statred == false) {
+    console.log("game is started");
+    started = true;
+  }
+});
