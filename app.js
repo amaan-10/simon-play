e6d55b7cf43ab5d69cf167e54c9c9f8c7fb96014
Amaan <amaanshaikh.gg@gmail.com>
2024-06-20 17:49:04 +0530

    game build

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let gameSeq = [];
-letuserSeq = [];
+let userSeq = [];
 
 let btns = ["green", "red", "yellow", "blue"];
 
@@ -25,13 +25,49 @@ function btnFlash(btn) {
 }
 
 function levelUp() {
+  userSeq = [];
   level++;
   h2.innerText = `Level ${level}`;
 
   let randIdx = Math.floor(Math.random() * 4);
   let randColor = btns[randIdx];
   let randBtn = document.querySelector(`.${randColor}`);
-  console.log(randIdx, randColor, randBtn);
-
+  // console.log(randIdx, randColor, randBtn);
+  gameSeq.push(randColor);
+  console.log(gameSeq);
   btnFlash(randBtn);
+}
+
+function checkAns(idx) {
+  if (userSeq[idx] === gameSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerText = "Game Over! Press any key to start again.";
+    reset();
+  }
+}
+
+function btnPress() {
+  let btn = this;
+  btnFlash(btn);
+
+  userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+  console.log(userSeq);
+
+  checkAns(userSeq.length - 1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+  btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+  started = false;
+  gameSeq = [];
+  userSeq = [];
+  level = 0;
 }
